6a1909359cc47693f0008ced0de9d58bffda140b
ignatievvv <ramorusbadrobot@gmail.com>
2021-03-27 10:28:42 +0300

    init: start hanoi-tower subtask

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let turnsNumber = Math.pow(2, disksNumber) - 1;
+  let turnTime = (60 * 60) / turnsSpeed;
+  let seconds = Math.floor(turnsNumber * turnTime);
+  let solution = {
+    turns: turnsNumber,
+    seconds: seconds,
+  };
+  return solution;
 };
