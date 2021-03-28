06a201a7ab08c6a83cd733a08a2589b43bce466e
ignatievvv <ramorusbadrobot@gmail.com>
2021-03-28 20:07:38 +0300

    init: start extended-repeater subtask

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let arr = [];
+  if (options.separator === undefined) {
+    options.separator = "+";
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = "|";
+  }
+  if (typeof str !== "string" && str !== undefined) {
+    str = `${str}`;
+  }
+  if (typeof options.addition !== "string" && options.addition !== undefined) {
+    options.addition = `${options.addition}`;
+  }
+  arr.push(str);
+  arr.push(options.addition);
+  for (let k = 1; k < options.additionRepeatTimes; k++) {
+    arr.push(options.additionSeparator);
+    arr.push(options.addition);
+  }
+  for (let i = 1; i < options.repeatTimes; i++) {
+    arr.push(options.separator);
+    arr.push(str);
+    arr.push(options.addition);
+    for (let k = 1; k < options.additionRepeatTimes; k++) {
+      arr.push(options.additionSeparator);
+      arr.push(options.addition);
+    }
+  }
+  return arr.join("");
 };
-  
